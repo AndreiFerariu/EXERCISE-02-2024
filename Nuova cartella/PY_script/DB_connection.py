598b5ed229ceb519a1e47404e8cd310c34d3cbdd
AndreiFerariu <94068694+AndreiFerariu@users.noreply.github.com>
2024-02-16 09:15:33 +0100

    part2

--- Nuova cartella/PY_script/DB_connection.py
+++ Nuova cartella/PY_script/DB_connection.py
@@ -1,15 +1,24 @@
-import sqlite3
+import mysql.connector
 
-class DatabaseConnection:
-    def __init__(self, db_name):
-        self.db_name = db_name
+class MySQLConnection:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
         self.connection = None
 
     def connect(self):
         try:
-            self.connection = sqlite3.connect(self.db_name)
-            print("Connessione al database avvenuta con successo!")
-        except sqlite3.Error as e:
+            self.connection = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+            if self.connection.is_connected():
+                print("Connessione al database avvenuta con successo!")
+        except mysql.connector.Error as e:
             print(f"Errore durante la connessione al database: {e}")
 
     def execute_query(self, query):
@@ -18,7 +27,13 @@ class DatabaseConnection:
             cursor.execute(query)
             self.connection.commit()
             print("Query eseguita con successo!")
-        except sqlite3.Error as e:
+        except mysql.connector.Error as e:
             print(f"Errore durante l'esecuzione della query: {e}")
+
+    def close_connection(self):
+        if self.connection:
+            self.connection.close()
+            print("Connessione al database chiusa.")
+
 
 
