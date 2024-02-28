b46d95cd61a177be4a1cb95f3e1e4866857eb7ad
AndreiFerariu <94068694+AndreiFerariu@users.noreply.github.com>
2024-02-28 10:36:34 +0100

    agg

--- Nuova cartella/DB_mng/DB_creation.sql
+++ Nuova cartella/DB_mng/DB_creation.sql
@@ -3,7 +3,7 @@ CREATE TABLE DBproducts.products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     marca VARCHAR(255) NOT NULL,
     nome VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
+    prezzo float NOT NULL
 );
 
 
