5ea0a76991ef8913a3a45206c44fa1cc14ee326c
AndreiFerariu <94068694+AndreiFerariu@users.noreply.github.com>
2024-02-28 10:38:29 +0100

    Update DB_insert.sql

--- Nuova cartella/DB_mng/DB_insert.sql
+++ Nuova cartella/DB_mng/DB_insert.sql
@@ -1,11 +1,1 @@
-INSERT INTO products (name, price, availability) VALUES
-    ('Laptop', 999.99, TRUE),
-    ('Smartphone', 599.99, FALSE),
-    ('Tablet', 399.99, TRUE),
-    ('Desktop', 1299.99, TRUE),
-    ('Smartwatch', 199.99, FALSE),
-    ('Headphones', 149.99, TRUE),
-    ('Camera', 799.99, TRUE),
-    ('Printer', 299.99, FALSE),
-    ('External Hard Drive', 129.99, TRUE),
-    ('Wireless Router', 79.99, TRUE);
+
