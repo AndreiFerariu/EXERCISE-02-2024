3c2b79942ae84df6304ab977b41d507b7981504a
AndreiFerariu <94068694+AndreiFerariu@users.noreply.github.com>
2024-02-28 11:16:57 +0100

    agg

--- Nuova cartella/PY_script/Products.py
+++ Nuova cartella/PY_script/Products.py
@@ -6,7 +6,7 @@ class Product:
     @staticmethod
     def establish_connection():
         try:
-            db_manager = DbManager("localhost", 3306, "ferariu_andrei", "masking.assurance.vitiating", "ferariu_andrei") #cambia
+            db_manager = DbManager("localhost", 3306, "ferariu_andrei", "masking.assurance.vitiating.", "ferariu_andrei_DBproducts") #cambia
             return db_manager.connect()
         except mysql.connector.Error as e:
             print("Errore durante la connessione al database:", str(e))
