9c39498a3fc0d12dbdc6e13aba27b04f23543224
AndreiFerariu <94068694+AndreiFerariu@users.noreply.github.com>
2024-02-28 10:50:40 +0100

    agg

--- Nuova cartella/PY_script/Products.py
+++ Nuova cartella/PY_script/Products.py
@@ -1,28 +1,95 @@
-from mysql_connection import MySQLConnection
+from dbmanager import DbManager
+import mysql.connector
 
 class Product:
-    def __init__(self, name, price, availability):
-        self.name = name
-        self.price = price
-        self.availability = availability
 
-class ProductManager:
-    def __init__(self, connection):
-        self.connection = connection
+    @staticmethod
+    def establish_connection():
+        try:
+            db_manager = DbManager("localhost", 3306, "ferariu_andrei", "masking.assurance.vitiating", "ferariu_andrei") #cambia
+            return db_manager.connect()
+        except mysql.connector.Error as e:
+            print("Errore durante la connessione al database:", str(e))
 
-    def create_product(self, product):
-        query = f"INSERT INTO Products (name, price, availability) VALUES ('{product.name}', {product.price}, {product.availability})"
-        self.connection.execute_query(query)
+    def __init__(self, id, nome, prezzo, marca):
+        self._id = id
+        self._nome = nome
+        self._prezzo = prezzo
+        self._marca = marca
+    
+    @property
+    def id(self):
+        return self._id
 
-    def read_products(self):
-        query = "SELECT * FROM Products"
-        cursor = self.connection.execute_query(query)
-        return cursor.fetchall()
+    @property
+    def nome(self):
+        return self._nome
 
-    def update_product(self, product):
-        query = f"UPDATE Products SET price = {product.price}, availability = {product.availability} WHERE name = '{product.name}'"
-        self.connection.execute_query(query)
+    @property
+    def prezzo(self):
+        return self._prezzo
 
-    def delete_product(self, product_name):
-        query = f"DELETE FROM Products WHERE name = '{product_name}'"
-        self.connection.execute_query(query)
+    @property
+    def marca(self):
+        return self._marca
+
+    @staticmethod
+    def get_all():
+        try: 
+            conn = Product.establish_connection()
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM products")
+            records = cursor.fetchall()
+            cursor.close()
+            return records
+        except mysql.connector.Error as e:
+            print("Errore durante la ricerca dei prodotti:", str(e))
+
+    @staticmethod
+    def find_by_id(id):
+        try:
+            conn = Product.establish_connection()
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM products WHERE id = %s", (id,))
+            row = cursor.fetchone()
+            conn.close()
+            if row:
+                return Product(id=row[0], nome=row[1], prezzo=row[2], marca=row[3])
+            else:
+                return None
+        except mysql.connector.Error as e:
+            print("Errore durante la ricerca del prodotto:", str(e))
+
+    @staticmethod
+    def create_product(product_data):
+        try:
+            conn = Product.establish_connection()
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO products (nome, prezzo, marca) VALUES (%s, %s, %s)", (product_data['nome'], product_data['prezzo'], product_data['marca']))
+            conn.commit()
+            product_id = cursor.lastrowid
+            conn.close()
+            product_data['id'] = product_id
+            return product_data
+        except mysql.connector.Error as e:
+            print("Errore durante la creazione del prodotto:", str(e))
+
+    def update_product(self, product_data):
+        try:
+            conn = Product.establish_connection()
+            cursor = conn.cursor()
+            cursor.execute("UPDATE products SET marca = %s, nome = %s, prezzo = %s WHERE id = %s", (product_data['marca'], product_data['nome'], product_data['prezzo'], self.id,))
+            conn.commit()
+            conn.close()
+        except mysql.connector.Error as e:
+            print("Errore durante l'aggiornamento del prodotto:", str(e))
+
+    def delete_product(self):
+        try:
+            conn = Product.establish_connection()
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM products WHERE id = %s", (self.id,))
+            conn.commit()
+            conn.close()
+        except mysql.connector.Error as e:
+            print("Errore durante l'eliminazione del prodotto:", str(e))
